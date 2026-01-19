6352890b37c52c691b8308faaed3b63271954a22
Simik Nainwal <simikjio@gmail.com>
2026-01-19 19:43:12 +0530

    String functions

---
+++ String Functions/string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String args[]){
+        String s = new String("Simik");
+
+        System.out.println(s);
+        s=s.concat("Nainwal");
+        System.out.println(s);
+    }
+}
---
+++ String Functions/string_functions.java
@@ -0,0 +1,16 @@
+public class string_functions {
+    public static void main(String args[]){
+        String s1="Hello";
+        String s2="hello";
+        // str=str.concat("World");
+
+        // System.out.println(str.charAt(3));
+        // System.out.println(s1.equals(s2));
+        // System.out.println(s1.equalsIgnoreCase(s2));
+        // System.out.println(s1.isEmpty());
+        // System.out.println(s2.indexOf('e'));
+        // System.out.println(s1.substring(2,4));
+        // System.out.println(s1.replace('l','z'));
+
+    }
+}
